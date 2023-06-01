fix unet normalization

caimages.py
@@ -67,19 +67,21 @@ class CaImagesDataset(torch.utils.data.Dataset):
             image, mask = sample['image'], sample['mask']
         
         # apply preprocessing
-        if self.preprocessing:
-            image = self.preprocessing(image)
-            _transform = []
-            _transform.append(transforms.ToTensor())
+        _transform = []
+        _transform.append(transforms.ToTensor())
 
-            img_size = 512
-            pad_left = (img_size - self.image_dim[0])//2
-            pad_top= (img_size - self.image_dim[1])//2
-            pad_right = img_size - self.image_dim[0] - pad_left
-            pad_bottom = img_size - self.image_dim[1] - pad_top
-            _transform.append(transforms.Pad(padding=(pad_left, pad_top, pad_right, pad_bottom), 
-                                            padding_mode='edge'))       
-            mask = transforms.Compose(_transform)(mask)
+        img_size = 512
+        width, height = self.image_dim
+        max_dim = max(img_size, width, height)
+        pad_left = (max_dim-width)//2
+        pad_right = max_dim-width-pad_left
+        pad_top = (max_dim-height)//2
+        pad_bottom = max_dim-height-pad_top
+        _transform.append(transforms.Pad(padding=(pad_left, pad_top, pad_right, pad_bottom), 
+                                        padding_mode='edge'))
+        _transform.append(transforms.Resize(interpolation=transforms.InterpolationMode.NEAREST_EXACT,size=(img_size, img_size)))  
+        mask = transforms.Compose(_transform)(mask)
+        image = transforms.Compose(_transform)(image)
             
         return image, mask
         
@@ -157,18 +159,18 @@ class UNet(nn.Module):
 
     def forward(self, x):
         """Forward pass of the UNet model
-        x: [1, 512, 512]
+        x: (16, 1, 512, 512)
         """
-        x, skip1_out = self.down_conv1(x) # x: [64, 256, 256], skip1_out: [64, 512, 512] (channels, height, width)
-        x, skip2_out = self.down_conv2(x) # x: [128, 128, 128], skip2_out: [128, 256, 256]
-        x, skip3_out = self.down_conv3(x) # x: [256, 64, 64], skip3_out: [256, 128, 128]
-        x, skip4_out = self.down_conv4(x) # x: [512, 32, 32], skip4_out: [512, 64, 64]
-        x = self.double_conv(x) # x: [1024, 32, 32]
-        x = self.up_conv4(x, skip4_out) # x: [512, 64, 64]
-        x = self.up_conv3(x, skip3_out)
-        x = self.up_conv2(x, skip2_out)
-        x = self.up_conv1(x, skip1_out)
-        x = self.conv_last(x)
+        x, skip1_out = self.down_conv1(x) # x: (16, 64, 256, 256), skip1_out: (16, 64, 512, 512) (batch_size, channels, height, width)
+        x, skip2_out = self.down_conv2(x) # x: (16, 128, 128, 128), skip2_out: (16, 128, 256, 256)
+        x, skip3_out = self.down_conv3(x) # x: (16, 256, 64, 64), skip3_out: (16, 256, 128, 128)
+        x, skip4_out = self.down_conv4(x) # x: (16, 512, 32, 32), skip4_out: (16, 512, 64, 64)
+        x = self.double_conv(x) # x: (16, 1024, 32, 32)
+        x = self.up_conv4(x, skip4_out) # x: (16, 512, 64, 64)
+        x = self.up_conv3(x, skip3_out) # x: (16, 256, 128, 128)
+        x = self.up_conv2(x, skip2_out) # x: (16, 128, 256, 256)
+        x = self.up_conv1(x, skip1_out) # x: (16, 64, 512, 512)
+        x = self.conv_last(x) # x: (16, 1, 512, 512)
         return x
 
 def get_training_augmentation():
@@ -214,17 +216,17 @@ def get_preprocessing(preprocessing_fn=None, image_dim=(512, 512)):
     _transform.append(transforms.ToTensor())
 
     img_size = 512
-    pad_left = (img_size - image_dim[0])//2
-    pad_top= (img_size - image_dim[1])//2
-    pad_right = img_size - image_dim[0] - pad_left
-    pad_bottom = img_size - image_dim[1] - pad_top
-    print("Adding padding")
-    print(pad_left, pad_top, pad_right, pad_bottom)
+    width, height = image_dim
+    max_dim = max(img_size, width, height)
+    pad_left = (max_dim-width)//2
+    pad_right = max_dim-width-pad_left
+    pad_top = (max_dim-height)//2
+    pad_bottom = max_dim-height-pad_top
     _transform.append(transforms.Pad(padding=(pad_left, pad_top, pad_right, pad_bottom), 
-                                     padding_mode='edge'))        
+                                    padding_mode='edge'))
+    _transform.append(transforms.Resize(interpolation=transforms.InterpolationMode.NEAREST_EXACT,size=(img_size, img_size)))       
     if preprocessing_fn:
         _transform.append(preprocessing_fn)
-    
 
     return transforms.Compose(_transform)
 

unet.py
@@ -29,12 +29,6 @@ if __name__ == '__main__':
     import segmentation_models_pytorch.utils.metrics
 
     ## DEFINE UNET MODEL
-    # Background is black, calcium is white
-    class_rgb_values = [
-        0, 255
-    ]
-    class_names=['background', 
-                 'calcium']
 
     x_train_dir=r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\unet_data_small\original\train"
     y_train_dir=r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\unet_data_small\ground_truth\train"
@@ -50,13 +44,13 @@ if __name__ == '__main__':
     train_dataset = CaImagesDataset(
         x_train_dir, y_train_dir, 
         augmentation=None,
-        preprocessing=get_preprocessing(preprocessing_fn=transforms.Normalize(mean=(0.5), std=(0.5)), image_dim = (width, height)),
+        preprocessing=None,
         image_dim = (width, height)
     )
     valid_dataset = CaImagesDataset(
         x_valid_dir, y_valid_dir, 
         augmentation=None,
-        preprocessing=get_preprocessing(preprocessing_fn=transforms.Normalize(mean=(0.5), std=(0.5)), image_dim = (width, height)),
+        preprocessing=None,
         image_dim = (width, height)
     )
 
@@ -67,52 +61,67 @@ if __name__ == '__main__':
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     target_width = 454 # change to max width of images in dataset (make sure dividable by 2)
     target_height = 546 # change to max height of images in dataset
-    # model = joblib.load(r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\models\unet\model4.pkl")
-    # image, gt_mask = train_dataset[2] # image and ground truth from test dataset
-    # print(image.shape, gt_mask.shape)
-    # print(image)
-    # plt.imshow(image.squeeze(0).numpy(), cmap='gray')
-    # plt.show()
-    # plt.imshow(gt_mask.squeeze(0).numpy(), cmap='gray')
-    # plt.show()
-    # x_tensor = image.to(DEVICE).unsqueeze(0)
-    # pred_mask = model(x_tensor)
-    # print(pred_mask)
-    # print(pred_mask.shape)
-    # plt.imshow(pred_mask.detach().numpy().squeeze(0).squeeze(0))
-    # plt.show()
-    # plt.imshow(pred_mask.detach().numpy().squeeze(0).squeeze(0), cmap="gray")
-    # plt.show()
-    # plt.imshow(TF.invert(pred_mask).detach().numpy().squeeze(0).squeeze(0), cmap="gray")
-    # plt.show()
-    # plt.imshow(TF.invert(pred_mask).detach().numpy().squeeze(0).squeeze(0))
-    # plt.show()
-    # exit()
+    TRAINING = True
+    TESTING = False
 
-    # Get train and val data loaders (load large amounts of data more efficiently)
+    if TRAINING:
+        model = UNet()
+        m=nn.Sigmoid()
+        print("Starting training...")
+        EPOCHS = 4
+        criterion = torch.nn.BCELoss()
+        optimizer = torch.optim.Adam(model.parameters(),lr=0.0001)
+        loss_list = [] # train and valid logs
+        for epoch in range(EPOCHS):
+            for i, data in enumerate(train_loader):
+                print("Progress: {:.2%}".format(i/len(train_loader)))
+                inputs, labels = data
+                pred = model(inputs)
+                # print(pred)
+                # print(pred.shape)
+                # exit()
+                loss = criterion(m(pred), labels) # calculate loss (binary cross entropy)
+                loss.backward() # calculate gradients (backpropagation)
+                optimizer.step() # update model weights (values for kernels)
+                print(f"Step: {i}, Loss: {loss}")
+                loss_list.append(loss)
+            print(f"Epoch: {epoch}, Loss: {loss}")
 
-    model = UNet()
+        joblib.dump(model, r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\models\unet\model9.pkl")
+        try:
+            joblib.dump(loss_list, r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\models\unet\loss_list9.pkl")
+        except:
+            print("Failed to save loss list")
 
-    print("Starting training...")
-    EPOCHS = 1
-    criterion = torch.nn.BCELoss()
-    optimizer = torch.optim.Adam(model.parameters(),lr=0.001)
-    m = nn.Sigmoid()
-    for i, data in enumerate(train_loader):
-        print("Progress: {:.2%}".format(i/len(train_loader)))
-        inputs, labels = data
-        pred = model(inputs)
-        print(pred.shape)
-        print(pred)
-        print(labels.shape)
-        print(labels)
-       
-        loss = criterion(m(pred), labels) # calculate loss (binary cross entropy)
-        loss.backward() # calculate gradients (backpropagation)
-        optimizer.step() # update model weights (values for kernels)
-        print(f"Epoch: {i}, Loss: {loss}")
 
-    joblib.dump(model, r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\models\unet\model6.pkl")
+    if TESTING:
+        model = joblib.load(r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\models\unet\model8.pkl")
+        image, gt_mask = train_dataset[2] # image and ground truth from test dataset
+        print(image.shape, gt_mask.shape)
+        print(image)
+        plt.imshow(image.squeeze(0).numpy(), cmap='gray')
+        plt.savefig(r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\results\unet\test.png")
+        plt.show()
+        plt.imshow(gt_mask.squeeze(0).numpy(), cmap='gray')
+        plt.savefig(r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\results\unet\test_gt.png")
+        plt.show()
+        x_tensor = image.to(DEVICE).unsqueeze(0)
+        pred_mask = model(x_tensor)
+        print(pred_mask)
+        print(pred_mask.shape)
+        plt.imshow(pred_mask.detach().numpy().squeeze(0).squeeze(0))
+        plt.savefig(r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\results\unet\test_pred.png")
+        plt.show()
+        plt.imshow(pred_mask.detach().numpy().squeeze(0).squeeze(0), cmap="gray")
+        plt.savefig(r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\results\unet\test_pred_gray.png")
+        plt.show()
+        plt.imshow(TF.invert(pred_mask).detach().numpy().squeeze(0).squeeze(0), cmap="gray")
+        plt.savefig(r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\results\unet\test_pred_gray_invert.png")
+        plt.show()
+        plt.imshow(TF.invert(pred_mask).detach().numpy().squeeze(0).squeeze(0))
+        plt.savefig(r"C:\Users\hozhang\Desktop\CaTracking\huayin_unet_lstm\results\unet\test_pred_invert.png")
+        plt.show()
+
 
     exit()
 